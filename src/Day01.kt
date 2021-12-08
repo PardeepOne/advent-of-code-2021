5963aa9cd9ad182362b7a0b257814364b9e1d996
PardeepOne <pardeeps460@gmail.com>
2021-12-08 18:31:14 +0100

    Add Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
+import java.util.stream.Collectors
+import javax.print.attribute.IntegerSyntax
+import kotlin.streams.toList
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val vals = input.stream().mapToInt(Integer::parseInt).toList();
+        var resCounter = 0
+        var prevElement = vals[0]
+        for (index in 0 until vals.size) {
+            val currentElem = vals[index]
+            if (index > 0 && prevElement < currentElem)
+                resCounter++
+            prevElement = currentElem
+        }
+        return resCounter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val vals = input.stream().mapToInt(Integer::parseInt).toList();
+        var resCounter = 0
+        var prevSum = 0
+        for (index in 0 until vals.size) {
+            var currentSum = 0
+            if (index + 2 < vals.size)
+                currentSum += vals[index] +vals[index+1]+vals[index+2]
+            if(index > 0 && currentSum > prevSum)
+                resCounter++
+            prevSum = currentSum
+        }
+        return resCounter
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
